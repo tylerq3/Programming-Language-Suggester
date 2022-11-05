f03b19e06deff0f6d3a69289eae000dda9e3faab
tyler quinn <tyler.quinn032@gmail.com>
2022-11-05 12:24:18 -0700

    WIP, taking a break

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,17 @@ function hideResultsAndError() {
 window.onload = function() {
   hideResultsAndError();
 
-  window.addEventListener("load", function() {
-    document.getElementById("radio-form").addEventListener("submit", handleRadio);
-  });
-  
-
-
+  document.querySelector("test").onsubmit = function(event) {
+    event.preventDefault();
+    hideResultsAndError();
 function handleRadio(event) {
   event.preventDefault();
-  const yes = document.querySelector("input[value='dancing']").value;
+  const yes = document.querySelector("input[name='dancing']").value;
   const no = document.querySelector("input[name='dancing']").value;
   const blue = document.querySelector("input[name='color']").value;
   const red = document.querySelector("input[name='color']").value;
   const gold = document.querySelector("input[name='color']").value;
-  const dogs = document.querySelector("input[value='animals']").value;
+  const dogs = document.querySelector("input[name='animals']").value;
   const cats = document.querySelector("input[name='animals']").value;
   const summer = document.querySelector("input[name='season']").value;
   const winter = document.querySelector("input[name='season']").value;
@@ -30,14 +27,26 @@ function handleRadio(event) {
   const no1 = document.querySelector("input[name='snake']").value;
 
   let result;
-  if (age && height) {
-    if () {
-      document.getElementById("python").removeAttribute("class");
+  if (yes && no && blue && red && gold && dogs && cats && summer && winter && yes1 && no1) {
+    if (yes , blue , dogs , summer , yes1) {
+      document.getElementById("dancing" ,"animals" , "color" , "snake" , "season" ).removeAttribute("class");
       result = "python";
-    } else if () {
+    } else if (no, blue, cats, winter, no1) {
       document.getElementById("ruby").removeAttribute("class");
       result = "ruby"
-    } else if () {
+    } else if (yes, red, dogs, summer, no1) {
       document.getElementById("swift").removeAttribute("class");
       result = "swift"
     }; 
+
+    document.getElementById("output").innerText = result; 
+
+
+    window.addEventListener("load", function() {
+    const form = document.getElementById("test");
+    form.addEventListener("submit", handleRadio);
+    });
+  };
+}; 
+};
+};
