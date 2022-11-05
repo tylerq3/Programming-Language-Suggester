e0a688351b6464419c453a701032817d3e2ea5e9
tyler quinn <tyler.quinn032@gmail.com>
2022-11-05 15:30:06 -0700

    finally get results, but undefined, still WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,44 @@
+
 function hideResults() {
-  document.getElementById("dance").setAttribute("class", "hidden");
-  document.getElementById("dogs").setAttribute("class", "hidden");
-  document.getElementById("season").setAttribute("class", "hidden"); 
-  document.getElementById("color").setAttribute("class", "hidden");
-  document.getElementById("snake").setAttribute("class", "hidden");
+  document.getElementById("run").setAttribute("class" , "hidden");
+  document.getElementById("pets").setAttribute("class" , "hidden");
+  document.getElementById("color").setAttribute("class" , "hidden");
+  document.getElementById("season").setAttribute("class" , "hidden");
+  document.getElementById("snake").setAttribute("class" , "hidden");
 }
 
+//UI Logic//
 function handleRadio(event) {
   event.preventDefault();
-  const yes = document.querySelector("yes").value;
-  const no = document.querySelector("no").value;
-  const blue = document.querySelector("blue").value;
-  const red = document.querySelector("red").value;
-  const summer = document.querySelector("summer").value;
-  const winter = document.querySelector("winter").value;
-  const yes1 = document.querySelector("yes1").value;
-  const no1 = document.querySelector("no1").value;
-  const dogs = document.querySelector("dogs").value;
-  const cats = document.querySelector("cats").value;
-
-
+  const yes = document.querySelector("input#yes")
+  const no = document.querySelector("input#no")
+  const dog = document.querySelector("input#dog")
+  const cat = document.querySelector("input#cat")
+  const blue = document.querySelector("input#blue")
+  const red = document.querySelector("input#red")
+  const summer = document.querySelector("input#summer")
+  const winter = document.querySelector("input#winter")
+  const YES = document.querySelector("input#yes")
+  const NO = document.querySelector("input#no")
+  
   let result;
-  if (yes, blue, summer, yes1, dogs) {
-    document.getElementById("swift").removeAttribute("class");
-    result = "swift";
-  } else if (no, red, winter, no1, cats) {
-    document.getElementById("ruby").removeAttribute("class");
-    result = "ruby";
-  } else if (yes, red, winter, no1, dogs) {
+  if (yes , dog , blue , summer , YES) {
     document.getElementById("python").removeAttribute("class");
     result = "python";
-
-    document.getElementById("output").innerText = result; 
+  } else if (no && cat && winter && red && NO) {
+    document.getElementById("ruby").removeAttribute("class");
+    result = "ruby";
+  } else if (yes && cat && summer && red && NO) {
+    document.getElementById("swift").removeAttribute("class");
+    result = "swift";
   }
 
 
 
+  document.getElementById("output").innerText = result; 
+}
+
 window.addEventListener("load", function() {
-  document.getElementById("radio-form").addEventListener("submit", handleRadio);
+  const form = document.getElementById("radio-form");
+  form.addEventListener("submit", handleRadio);
 });
-};
