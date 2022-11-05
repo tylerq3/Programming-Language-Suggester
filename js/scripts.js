1537f86332fc8408dd8659f93a872c9e48c5aee2
tyler quinn <tyler.quinn032@gmail.com>
2022-11-05 16:36:28 -0700

    getting undefined results still WIP

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,19 @@ function hideResults() {
 //UI Logic//
 function handleRadio(event) {
   event.preventDefault();
-  const yes = document.querySelector("input#yes")
-  const no = document.querySelector("input#no")
-  const dog = document.querySelector("input#dog")
-  const cat = document.querySelector("input#cat")
-  const blue = document.querySelector("input#blue")
-  const red = document.querySelector("input#red")
-  const summer = document.querySelector("input#summer")
-  const winter = document.querySelector("input#winter")
-  const YES = document.querySelector("input#yes")
-  const NO = document.querySelector("input#no")
+  const yes = document.querySelector("input[name='yes']")
+  const no = document.querySelector("input[name='no']")
+  const dog = document.querySelector("input[name='dog']")
+  const cat = document.querySelector("input[name='cat']")
+  const blue = document.querySelector("input[name='blue']")
+  const red = document.querySelector("input[name='red']")
+  const summer = document.querySelector("input[name='summer']")
+  const winter = document.querySelector("input[name='winter']")
+  const YES = document.querySelector("input[name='yes1']")
+  const NO = document.querySelector("input[name='no1']")
   
   let result;
-  if (yes , dog , blue , summer , YES) {
+  if (yes) {
     document.getElementById("python").removeAttribute("class");
     result = "python";
   } else if (no && cat && winter && red && NO) {
