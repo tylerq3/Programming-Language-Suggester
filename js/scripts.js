61f91fe8580446591b834f386ccc71a33f707df5
tyler quinn <tyler.quinn032@gmail.com>
2022-11-05 17:58:58 -0700

    updated readme, add imgs, updated css, JS still WIP and stuck

--- js/scripts.js
+++ js/scripts.js
@@ -10,21 +10,21 @@ function hideResults() {
 //UI Logic//
 function handleRadio(event) {
   event.preventDefault();
-  const yes = document.querySelector("input[name='yes']");
-  const no = document.querySelector("input[name='no']");
-  const dog = document.querySelector("input[name='dog']");
-  const cat = document.querySelector("input[name='cat']");
-  const blue = document.querySelector("input[name='blue']");
-  const red = document.querySelector("input[name='red']");
-  const summer = document.querySelector("input[name='summer']");
-  const winter = document.querySelector("input[name='winter']");
-  const YES = document.querySelector("input[name='yes1']");
-  const NO = document.querySelector("input[name='no1']");
+  const yes = document.querySelector("input[run='yes'");
+  const no = document.querySelector("input[run='no']");
+  const dog = document.querySelector("input[pets='dog']");
+  const cat = document.querySelector("input[pets='cat']");
+  const blue = document.querySelector("input[color='blue']");
+  const red = document.querySelector("input[color='red']");
+  const summer = document.querySelector("input[season='summer']");
+  const winter = document.querySelector("input[season='winter']");
+  const YES = document.querySelector("input[snake='yes1']");
+  const NO = document.querySelector("input[snake='no1']");
   
   let result;
-  if ("snake"===yes && "season"===summer && "color"===blue && "pets"===dog && "run"===yes) {
-    document.getElementById("python").removeAttribute("class");
-    result = "python"; 
+  if (yes && dog && blue && summer && YES) {
+    document.getElementById("python").removeAttribute("class"); 
+    result = "python"
   } else if (no && cat && winter && red && NO) {
     document.getElementById("ruby").removeAttribute("class");
     result = "ruby";
